abb5f981e8e7a2a4b9cbf9070ef6d2c8e6408826
msy <msy780022@gmail.com>
2020-07-02 07:46:17 +0000

    fixed caution i think

--- db/migrate/20200701102926_create_calcdata.rb
+++ db/migrate/20200701102926_create_calcdata.rb
@@ -1,7 +1,6 @@
 class CreateCalcdata < ActiveRecord::Migration[5.2]
   def change
     create_table :calcdata do |t|
-      t.column :id, 'PRIMARY KEY AUTO_INCREMENT'
       t.integer :classnum
       t.string :gradekind
 
